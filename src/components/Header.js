faf7c1c2c3ef663aabbc589ef336ba0031e5d3ac
Lauryn <lajeff77@gmail.com>
2023-07-12 14:33:52 -0700

    building skeleton of homepage

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Lauryn Jefferson</h1>
+        <nav>
+            <ul>
+                <a href='#about'><li>About</li></a>
+                <a href='#projects'><li>Projects</li></a>
+                <a href='#contact'><li>Contact</li></a>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+        <body>
+            <h2 id='about'>About me</h2>
+            <p>This is about me.</p>
+            <h2 id='projects'>Projects</h2>
+            <p>These are my projects.</p>
+            <h2 id='contact'>Contact</h2>
+            <p>You can contact me here.</p>
+        </body>
+    </div>
+  )
+}
+
+export default Homepage
